9759a13f887e779338200ac105567285e98d032f
David Björklund <david.bjorklund@gmail.com>
2016-04-10 23:09:26 +0200

    don't bundle already bundled images

---
+++ test/bundle-images-test.js
@@ -0,0 +1,39 @@
+import test from 'ava';
+import 'babel-core/register';
+import bundleImages from '../lib/bundle-images';
+
+test('bundleImages() simple', t => {
+  const components = [{
+    role: 'photo',
+    URL: 'http://example.com/beep-boop.jpg'
+  }];
+  const expectedComponents = [{
+    role: 'photo',
+    URL: 'bundle://image-0'
+  }];
+  const expectedBundlesToUrls = {
+    'image-0': 'http://example.com/beep-boop.jpg'
+  };
+  const actualBundlesToUrls = bundleImages(components);
+  const actualComponents = components;
+
+  t.deepEqual(actualBundlesToUrls, expectedBundlesToUrls);
+  t.deepEqual(actualComponents, expectedComponents);
+});
+
+test('bundleImages() on bundled image', t => {
+  const components = [{
+    role: 'photo',
+    URL: 'bundle://beep-boop.jpg'
+  }];
+  const expectedComponents = [{
+    role: 'photo',
+    URL: 'bundle://beep-boop.jpg'
+  }];
+  const expectedBundlesToUrls = {};
+  const actualBundlesToUrls = bundleImages(components);
+  const actualComponents = components;
+
+  t.deepEqual(actualBundlesToUrls, expectedBundlesToUrls);
+  t.deepEqual(actualComponents, expectedComponents);
+});
